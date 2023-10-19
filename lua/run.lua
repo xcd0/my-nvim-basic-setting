b42cd565e5b8bafc12bce1eddfff79c13b95d392
xcd0 <xcd0@xcd0.com>
2023-10-19 13:08:47 +0900

    fix

--- lua/run.lua
+++ lua/run.lua
@@ -1,6 +1,6 @@
 -- キャッシュされたモジュールを削除
 package.loaded['myNvimBasicSetting'] = nil
-package.loaded['myNvimBasicSetting/testModule'] = nil
+package.loaded['myNvimBasicSetting/myNvimBasicSetting'] = nil
 
 -- モジュールを呼び出す
 --local setting_plugin = require('myNvimBasicSetting')
